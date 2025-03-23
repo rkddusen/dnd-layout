07d6c59388a0ff7b33cef99615a05aec545936fc
rkddusen <k99endus@naver.com>
2025-03-23 17:28:07 +0900

    기존 폴더 정리

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,9 @@
-import { useState } from 'react';
-import { DndContext, DragEndEvent } from '@dnd-kit/core';
-
-import { Droppable } from './Droppable';
-import { Draggable } from './Draggable';
+import { Home } from './dnd/Home';
 
 export const App = () => {
-  const [isDropped, setIsDropped] = useState<boolean>(false);
-  const draggableMarkup = <Draggable>Drag me</Draggable>;
-
-  const handleDragEnd = (event: DragEndEvent): void => {
-    if (event.over?.id === 'droppable') {
-      setIsDropped(true);
-    }
-  };
-
   return (
-    <DndContext onDragEnd={handleDragEnd}>
-      {!isDropped ? draggableMarkup : null}
-      <Droppable>{isDropped ? draggableMarkup : 'Drop here'}</Droppable>
-    </DndContext>
+    <>
+      <Home />
+    </>
   );
 };
---
+++ src/dnd/Home.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import { DndContext, DragEndEvent } from '@dnd-kit/core';
+
+import { Droppable } from './Droppable';
+import { Draggable } from './Draggable';
+
+export const Home = () => {
+  const [isDropped, setIsDropped] = useState<boolean>(false);
+  const draggableMarkup = <Draggable>Drag me</Draggable>;
+
+  const handleDragEnd = (event: DragEndEvent): void => {
+    if (event.over?.id === 'droppable') {
+      setIsDropped(true);
+    }
+  };
+
+  return (
+    <DndContext onDragEnd={handleDragEnd}>
+      {!isDropped ? draggableMarkup : null}
+      <Droppable>{isDropped ? draggableMarkup : 'Drop here'}</Droppable>
+    </DndContext>
+  );
+};
